84e46970e9ca6e58d105830cc5108f6691f499ec
dragon <1563829760@qq.com>
2018-04-15 23:44:53 +0800

    add some files about vue component

---
+++ js/index.js
@@ -0,0 +1,16 @@
+window.onload=function(){
+    "use strict";
+    Vue.component(
+        "dragon-template",{
+            template:`
+            `
+        }
+    )
+    var data={}
+    var vm = new Vue({
+        el:".caontainer",
+        data:data,
+        methods:{},
+        
+    })
+}
